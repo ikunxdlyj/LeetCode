1ae5ec4d39774b6cff8ea92eb1df02803d2046b7
yjlei <yjlei@stu.xidian.edu.cn>
2025-08-27 09:20:25 +0800

    8.27提交

--- src/class4/class4_2/OrangesRotting.java
+++ src/class4/class4_2/OrangesRotting.java
@@ -1,48 +1,47 @@
 package class4.class4_2;
 
 public class OrangesRotting {
-    public static int[][] queue = new int[101][2];
     public static int l, r, size, level;
     public static final int[][] move = {
             {-1, 0},
             {1, 0},
-            {0, -1},
-            {0, 1}
+            {0, 1},
+            {0, -1}
     };
+    public static int[][] queue = new int[11 * 11][2];
 
     public int orangesRotting(int[][] grid) {
-        l = r = size = level = 0;
+        l = r = level = 0;
         int freshCnt = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
                     freshCnt++;
-                }
-                if (grid[i][j] == 2) {
+                } else if (grid[i][j] == 2) {
                     queue[r][0] = i;
                     queue[r++][1] = j;
-                    size++;
                 }
             }
         }
-        while (l < r) {
+        while (l < r && freshCnt > 0) {
             level++;
-            for (int i = 0; i < size; i++) {
-                int[] cur = queue[l++];
-                size--;
+            int curLevelSize = r - l;
+            for (int i = 0; i < curLevelSize; i++) {
+                int x = queue[l][0];
+                int y = queue[l++][1];
                 for (int j = 0; j < 4; j++) {
-                    int nx = cur[0] + move[j][0];
-                    int ny = cur[1] + move[j][1];
-                    if (nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && grid[nx][ny] == 1) {
+                    int nx = x + move[j][0];
+                    int ny = y + move[j][1];
+                    if (nx >= 0 && ny >= 0 && nx < grid.length && ny < grid[0].length && grid[nx][ny] == 1) {
                         grid[nx][ny] = 2;
                         freshCnt--;
                         queue[r][0] = nx;
                         queue[r++][1] = ny;
-                        size++;
                     }
                 }
             }
         }
         return freshCnt > 0 ? -1 : level;
     }
+
 }
